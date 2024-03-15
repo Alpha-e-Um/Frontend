10c74d8058ae4e73795d037dfed124798c40a9a9
jihunchoi <geehoon62017@gmail.com>
2024-03-16 00:47:01 +0900

    style. navigation, introduce card 스타일 변경

--- src/components/Introduce/introduce.js
+++ src/components/Introduce/introduce.js
@@ -11,23 +11,37 @@ const Introduce = () => {
     <Container>
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
+          backgroundColor: "#714adf",
+          width: "100%",
+          height: "100%",
           justifyContent: "center",
-          alignItems: "start",
-          maxWidth: "1000px",
+          display: "flex",
+          margin: "30px 60px",
+          borderRadius: "26px",
         }}
       >
-        <Slogan>세상의 모든 팀플을 위한 연결의 시작!</Slogan>
-        <Description>
-          이음이 제공하는 자유롭고 편안한 프로젝트 서비스,
-        </Description>
-        <Description>
-          팀원, 팀플을 구하는 새로운 전환점과 AI서비스를 이용해보세요
-        </Description>
-        <GetTeamButton>
-          <ButtonText>첫 팀 구하기</ButtonText>
-        </GetTeamButton>
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "start",
+            maxWidth: "1000px",
+          }}
+        >
+          <Slogan>세상의 모든 팀플을 위한 연결의 시작!</Slogan>
+          <Description>
+            이음이 제공하는 자유롭고 편안한 프로젝트 서비스,
+          </Description>
+          <Description>
+            팀원, 팀플을 구하는 새로운 전환점과 AI서비스를 이용해보세요
+          </Description>
+          <GetTeamButton>
+            <ButtonText>첫 팀 구하기</ButtonText>
+          </GetTeamButton>
+        </div>
       </div>
     </Container>
   );
--- src/components/Introduce/styles.js
+++ src/components/Introduce/styles.js
@@ -2,37 +2,38 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  background-color: #a6fcdc;
   height: 100%;
   display: flex;
   justify-content: center;
 `;
 
 export const Slogan = styled.label`
-  font-size: 64px;
-  font-weight: 600;
+  color: white;
+  font-size: 40px;
+  font-weight: 700;
   margin-top: 80px;
   margin-bottom: 20px;
 `;
 
 export const Description = styled.label`
-  font-size: 32px;
-  font-weight: 400;
+  color: white;
+  font-size: 24px;
+  font-weight: 500;
   margin-bottom: 6px;
 `;
 
 export const GetTeamButton = styled.button`
   all: unset;
-  background-color: #376fff;
+  background-color: #ffffff;
   padding: 11px 30px;
-  color: white;
   margin-top: 60px;
-  margin-bottom: 100px;
+  margin-bottom: 80px;
   border-radius: 12px;
   cursor: pointer;
 `;
 
 export const ButtonText = styled.label`
+  color: #6632e9;
   font-size: 24px;
   font-weight: 700;
   cursor: pointer;
