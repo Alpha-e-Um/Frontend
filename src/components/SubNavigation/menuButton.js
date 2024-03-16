fabfc515c45b9334b583dd9c03db97754e046890
jihunchoi <geehoon62017@gmail.com>
2024-03-16 18:35:21 +0900

    feature. subnavigation 제작

---
+++ src/components/SubNavigation/menuButton.js
@@ -0,0 +1,19 @@
+import { IconBox } from "./styles";
+
+const MenuButton = (props) => {
+  console.log(props.imgUrl);
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", cursor: "pointer" }}
+    >
+      <IconBox>
+        <img src={props.imgUrl} alt="icon" width={42} />
+      </IconBox>
+      <label style={{ fontSize: "18px", fontWeight: "700", cursor: "pointer" }}>
+        {props.description}
+      </label>
+    </div>
+  );
+};
+
+export default MenuButton;
