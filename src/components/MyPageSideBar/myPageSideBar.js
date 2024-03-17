f7b051fa7673d78de3912b51c4e34cd49c109df1
sotetero <sotetero11@gmail.com>
2024-03-17 14:05:10 +0900

    feature. 마이페이지 side뷰 제작

--- src/components/MyPageSideBar/myPageSideBar.js
+++ src/components/MyPageSideBar/myPageSideBar.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
-import { Container, SelectPage } from "./styles";
+import { Title, Container, SelectPage, NotSelectPage } from "./styles";
 
 const MyPageSideBar = (props) => {
-  const [curPage, setCurPage] = useState(0);
+  const [curPage, setCurPage] = useState(5);
+
+  var arr = ["내정보", "즐겨찾기", "지원현황", "팀관리", "이력서", "회원탈퇴"];
 
   return (
     <div>
+      <Title>마이페이지</Title>
       <Container>
-        <SelectPage>내 정보</SelectPage>
+        {arr.map((item, index) => {
+          const ItemComponent = curPage === index ? SelectPage : NotSelectPage;
+
+          return <ItemComponent key={index}>{item}</ItemComponent>;
+        })}
       </Container>
     </div>
   );
--- src/components/MyPageSideBar/styles.js
+++ src/components/MyPageSideBar/styles.js
@@ -1,16 +1,43 @@
 import styled from "styled-components";
 
+export const Title = styled.div`
+  position: absolute;
+  top: 114px;
+  left: 159px;
+  font-size: 32px;
+  font-weight: 700;
+`;
+
 export const Container = styled.div`
   position: absolute;
-  top: 150px;
-  left: 150px;
-  width: 292px;
-  height: 635px;
+  display: flex;
+  flex-direction: column;
+  top: 201px;
+  left: 139px;
+  width: 212px;
+  height: 592px;
   border: solid;
   border-color: #c9c9c9;
   border-radius: 12px;
+  padding-top: 30px;
 `;
 
-export const SelectPage = styled.label``;
+export const SelectPage = styled.button`
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 70px;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+`;
 
-export const NotSelectPage = styled.label``;
+export const NotSelectPage = styled.button`
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 70px;
+
+  border: none;
+  background-color: #ffffff;
+  color: #a9a9a9;
+  cursor: pointer;
+`;
