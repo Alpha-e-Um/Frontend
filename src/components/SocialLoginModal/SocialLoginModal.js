30c2b9a5e7a72740b60e34498655090abc99624b
jihunchoi <geehoon62017@gmail.com>
2024-03-25 14:55:27 +0900

    feat: axios 업데이트, 소셜로그인 함수 작성

--- src/components/SocialLoginModal/SocialLoginModal.js
+++ src/components/SocialLoginModal/SocialLoginModal.js
@@ -12,6 +12,21 @@ import {
 } from "./styles";
 
 const SocialLoginModal = ({ setClickLoginButton }) => {
+  const loginWithKakao = () => {
+    window.location.href = "/oauth2/authorization/kakao";
+    console.log(123);
+  };
+
+  const loginWithGoogle = () => {
+    window.location.href = "/oauth2/authorization/google";
+    console.log(123);
+  };
+
+  const loginWithNaver = () => {
+    window.location.href = "/oauth2/authorization/naver";
+    console.log(123);
+  };
+
   return (
     <div>
       <ContainerOut>
@@ -19,7 +34,7 @@ const SocialLoginModal = ({ setClickLoginButton }) => {
           <Title>이음</Title>
           <Explanation>간편하게 시작하는 회원가입</Explanation>
           <KakaoButton>카카오톡으로 진행하기</KakaoButton>
-          <GoogleButton>구글로 진행하기</GoogleButton>
+          <GoogleButton onClick={loginWithGoogle}>구글로 진행하기</GoogleButton>
           <NaverButton>네이버로 진행하기</NaverButton>
           <ExitButton
             onClick={() => {
