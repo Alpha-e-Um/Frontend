65a2ff8fc05886b6df1b00bb9bbed19db0362fc2
jihunchoi <geehoon62017@gmail.com>
2024-05-29 15:18:11 +0900

    feat: #52-팀 등록시 로고(사진) 추가

--- src/api/teamAPI.js
+++ src/api/teamAPI.js
@@ -2,7 +2,33 @@ import { axiosWithAuth } from "./axios";
 
 export const teamAPI = {
   postNewTeam(data) {
-    return axiosWithAuth.post("/team", data);
+    const formData = new FormData();
+    formData.append(
+      "json",
+      new Blob(
+        [
+          JSON.stringify({
+            name: data.name,
+            introduction: data.introduction,
+            domain: data.domain,
+            location: data.location,
+            phoneNumber: data.phoneNumber,
+            email: data.email,
+          }),
+        ],
+        { type: "application/json" },
+      ),
+    );
+
+    if (data.profileImage) {
+      formData.append("logo", data.profileImage);
+    }
+
+    return axiosWithAuth.post("/team", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
   },
   getTeamById(id) {
     return axiosWithAuth.get(`/team/${id}`);
