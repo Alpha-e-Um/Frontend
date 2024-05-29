ff451e63238d172ac1b4fb3bc755fe1ed5d3b45c
jihunchoi <geehoon62017@gmail.com>
2024-05-29 20:25:21 +0900

    feat: #52-팀카드 수정

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 24px;
+  padding: 28px;
   display: flex;
   flex-direction: column;
   border-radius: 24px;
   border: 1px solid #c9c9c9;
-  max-width: 360px;
+  background-color: #ffffff;
+  max-width: 100%;
+  overflow: hidden;
 `;
 
 export const Title = styled.label`
   font-size: 24px;
   color: #6d758f;
   font-weight: 700;
+  cursor: pointer;
 `;
 
 export const Position = styled.label`
@@ -29,7 +32,7 @@ export const StateLabel = styled.label`
 `;
 
 export const TimeLabel = styled.label`
-  border-left: 1px solid;
+  border-left: 1px solid #c9c9c9;
   font-size: 14px;
   padding-left: 10px;
   color: #6d758f;
@@ -55,28 +58,45 @@ export const Description = styled.label`
 export const InteractBox = styled.div`
   display: flex;
   justify-content: space-between;
-  border-top: 1px solid #c9c9c9;
   align-items: center;
   margin-top: 20px;
   padding-top: 20px;
+  border-top: 1px solid #c9c9c9;
 `;
 
 export const ButtonBox = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 8px;
+  overflow-x: auto;
+  white-space: nowrap;
+  max-width: 100%;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
 `;
+
+// ButtonBox::-webkit-scrollbar {
+//   display: none; /* Chrome, Safari, and Opera */
+// }
 
 export const StyledButton = styled.button`
   all: unset;
-  cursor: pointer;
   background-color: #3376f6;
-  border-radius: 18px;
+  border-radius: 16px;
   padding: 6px 14px;
   color: white;
   font-size: 14px;
   font-weight: 500;
+  white-space: nowrap;
 `;
 
 export const LikeButton = styled.img`
   cursor: pointer;
+  margin-left: 16px;
+`;
+
+export const LogoImg = styled.img`
+  cursor: pointer;
+  width: 100px;
+  height: 100px;
+  border-radius: 16px;
 `;
--- src/components/CardGrid/styles.js
+++ src/components/CardGrid/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   gap: 52px 26px;
 `;
 
